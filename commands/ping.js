d5cb003ddbabaaf65bd1adfac4b96ea509a5169d
Ryo <46572489+sRyoh@users.noreply.github.com>
2020-09-04 21:05:29 -0400

    Delete ping.js

--- commands/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name : 'ping',
-    aliases : ['pong', 'pingpong'],
-    cooldown : 5,
-    description : 'Ping!',
-    execute(message, args) {
-        message.channel.send('Pong');
-    },
-};
